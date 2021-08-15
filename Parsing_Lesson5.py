9350cac4bb8f23d07cf5d5c4770137b38878cc4f
schepiga <76489817+schepiga@users.noreply.github.com>
2021-08-15 23:48:27 +0300

    Update Parsing_Lesson5.py

--- Parsing_Lesson5.py
+++ Parsing_Lesson5.py
@@ -1,32 +1,27 @@
+import ast
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import time
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver import ActionChains
+from pymongo import MongoClient
 
 chrome_options = Options()
 chrome_options.add_argument("start-maximized")
 
 driver = webdriver.Chrome(executable_path='./chromedriver.exe', options=chrome_options)
-driver.get("https://www.mvideo.ru/")
+driver.get("https://www.mvideo.ru/?cityId=CityCZ_975")
 
-# button_popup = driver.find_element_by_class_name('main-container')
-button_popup = driver.find_element_by_class_name('tooltipster-bottom')
-button_popup.click()
-
-# sales_button = driver.find_element_by_xpath('//a[@class=close)]')
-# actions.move_to_element(sales_button).perform()
-
-wait = WebDriverWait(driver, 5)
-
+wait = WebDriverWait(driver, 10)
+button_popup = driver.find_element_by_class_name('span[@data-close="true"]')
 while True:
     try:
         close_button = wait.until(EC.presence_of_element_located(button_popup))
-    except Exception:
-        break
+    except Exception as e:
+        print(e)
     else:
         button_popup.clik()
 
@@ -34,17 +29,24 @@ actions = ActionChains(driver)
 goods = driver.find_element_by_xpath('//div[contains(h2, "Новинки")]')
 actions.move_to_element(goods).perform()
 
-wait = WebDriverWait(driver, 5)
-
 while True:
     try:
-        next_button = wait.until(EC.element_to_be_clickable(By.XPATH, '//div[contains(h2, "Новинки")]'))
-    except Exception:
+        next_button = wait.until(EC.element_to_be_clickable(
+            (By.XPATH, '//div[contains(h2, "Новинки")]/../..//a[contains(@class, "next-btn")]')
+        )
+        )
+    except Exception as e:
         break
     else:
         next_button.click()
 
+new_goods = driver.find_element_by_xpath(
+    'div[contains(h2, "Новинки")]/../..//a[contains(@class, "fl-product-tile-picture")]')
 
+client = MongoClient('localhost', 27017)
+new_goods_db = client['mvideo15082021']
 
-
-
+for itm in new_goods:
+    name = itm.get_attribete('data-product-info')
+    goods_list = ast.literal_eval(name)
+    new_goods_db.goods.inset_one(goods_list)
